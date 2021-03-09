cbf1db1421a54afde2e0ec8f60c46d0d24831446
NetZorro <deniskodua@gmail.com>
2021-03-09 22:54:19 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import styles from "./app.scss";
 
-const param = 1;
-
 const   App   :   React.FC = () => {
     return <div className={styles.helloText}>Hello Travel App
     
