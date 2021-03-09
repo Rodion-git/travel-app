06f6e29b0aa35bebdd88098f3dd5c51e722a5837
NetZorro <deniskodua@gmail.com>
2021-03-09 20:28:10 +0400

    husky test
    Настройка husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import styles from "./app.scss";
 
-const App: React.FC = () => {
-  return <div className={styles.helloText}>Hello Travel App</div>;
+const App    = () => {
+    return <div className={styles.helloText}>Hello Travel App</div>;
 };
 
-export default App;
+
+
+export   default  App;
