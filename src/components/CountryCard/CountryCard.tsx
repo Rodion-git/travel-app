104598fd2e9cc5034d25f653a86d17fdff721e2d
Kostik <ks@wintecs.by>
2021-03-09 20:34:52 +0300

    feat: add Header, Footer

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -5,6 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
     cardGrid: {
@@ -15,12 +16,20 @@ const useStyles = makeStyles((theme) => ({
         height: "100%",
         display: "flex",
         flexDirection: "column",
+        borderRadius: "10px",
+
+        "&:hover": {
+            border: "3px solid blue",
+            cursor: "pointer",
+        },
     },
     cardMedia: {
         paddingTop: "56.25%", // 16:9
     },
     cardContent: {
+        color: "white",
         flexGrow: 1,
+        backdropFilter: "blur(15px)",
     },
 }));
 
@@ -29,19 +38,26 @@ const CountryCard: React.FC = () => {
 
     return (
         <Grid item key="1" xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-                <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Название страны"
-                />
-                <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Название старны
-                    </Typography>
-                    <Typography>Столица: Название столицы</Typography>
-                </CardContent>
-            </Card>
+            <Link href="#">
+                <Card className={classes.card}>
+                    <CardMedia
+                        className={classes.cardMedia}
+                        image="https://source.unsplash.com/random"
+                        title="Название страны"
+                    />
+                    <CardContent
+                        className={classes.cardContent}
+                        style={{
+                            backgroundImage: `url(https://source.unsplash.com/random)`,
+                        }}
+                    >
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Название старны
+                        </Typography>
+                        <Typography>Столица: Название столицы</Typography>
+                    </CardContent>
+                </Card>
+            </Link>
         </Grid>
     );
 };
