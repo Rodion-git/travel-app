5b1ee3631849a1eb20e0e36609c99dc2aeaaaae0
NetZorro <deniskodua@gmail.com>
2021-03-09 22:48:14 +0400

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import styles from "./app.scss";
 
-const test1    = "test test test test test   1 ss";
-
 const   App   :   React.FC = () => {
     return <div className={styles.helloText}>Hello Travel App
     
